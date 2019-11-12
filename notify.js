0c1aab0306bafa1e056d39b70d2144ec000e3bed
TheRunningStef <leondstef@gmail.com>
2019-11-12 13:51:38 +0000

    updated nebula

--- notify.js
+++ notify.js
@@ -1,58 +1,70 @@
 window.onload = function() {
 	var uid = [];
 	var link = "";
-	
+
 	last =  function(array, n) {
-		if (array == null) 
+		if (array == null)
 			return void 0;
-		if (n == null) 
+		if (n == null)
 			return array[array.length - 1];
-		return array.slice(Math.max(array.length - n, 0));  
+		return array.slice(Math.max(array.length - n, 0));
 		};
-	function notifClick(id) {
-		var d = document.getElementById(id);
-		d.className += "notificationDismiss";
-	}
-	
+
 	function newNotif(notDesc) {
+
 		var parser = new DOMParser();
-		var xmlDoc = parser.parseFromString(notDesc,"text/xml");	
-	
+		var xmlDoc = parser.parseFromString(notDesc,"text/xml");
+
 		var div = document.createElement('div');
+		var autd = document.createElement('div');
 		var h2 = document.createElement('h2');
 		var h3 = document.createElement('h3');
 		var p = document.createElement('p');
-		
+
 		var title = xmlDoc.getElementsByTagName("title")[0].childNodes[0].nodeValue;
 		var appName = xmlDoc.getElementsByTagName("appName")[0].childNodes[0].nodeValue;
 		var desc = xmlDoc.getElementsByTagName("description")[0].childNodes[0].nodeValue;
-		link = xmlDoc.getElementsByTagName("link")[0].childNodes[0].nodeValue;
-		
+		var link = xmlDoc.getElementsByTagName("link")[0].childNodes[0].nodeValue;
+
 		h2.innerHTML = title;
 		h3.innerHTML = appName;
 		p.innerHTML = desc;
-		
-		div.appendChild(h3);
+
+		autd.appendChild(h3);
+		div.appendChild(autd);
 		div.appendChild(h2);
 		div.appendChild(p);
 		document.body.appendChild(div);
-		
+
 		if (uid.length == 0) {
 			div.setAttribute("id", "not0");
 			uid.push(0);
 		} else {
 			var newID = last(uid) + 1;
 			var newIdStr = newID.toString();
-			div.setAttribute("id", "not" + newIdStr);
+			var ID = "not" + newIdStr;
+			div.setAttribute("id", ID);
 			uid.push(newID);
 		}
-		
+
 		div.setAttribute("class", "notification");
-		div.setAttribute("onclick", "notifClick(not" + newIdStr + ")");
+		autd.setAttribute("class", "appTitle");
+
+		if (link == "auto_local") {
+			div.setAttribute("onclick", "openTool(" + appName + ", " + ID + ");");
+		}
 	}
-	
-	newNotif("<notification version='0.1'><title>Alpha</title><appName>notify.js</appName><description>Hello, World!</description><link>index.html</link></notification>");
-	newNotif("<notification version='0.1'><title>Beta</title><appName>notify.js</appName><description>It's Alive!</description><link>index.html</link></notification>");
-	newNotif("<notification version='0.1'><title>Gamma</title><appName>notify.js</appName><description>Bonj</description><link>index.html</link></notification>");
-	newNotif("<notification version='0.1'><title>Delta</title><appName>notify.js</appName><description>ayo wagwan g</description><link>index.html</link></notification>");
+
+	newNotif("<notification version='0.1'><title>Alpha</title><appName>oyster.notification-test</appName><description>Hello, World!</description><link>auto_local</link></notification>");
+	newNotif("<notification version='0.1'><title>Beta</title><appName>oyster.notification-test</appName><description>It's Alive!</description><link>auto_local</link></notification>");
+	newNotif("<notification version='0.1'><title>Gamma</title><appName>oyster.notification-test</appName><description>Bonj</description><link>auto_local</link></notification>");
+	newNotif("<notification version='0.1'><title>Delta</title><appName>oyster.notification-test</appName><description>ayo wagwan g</description><link>auto_local</link></notification>");
+}
+
+function openTool(_tool_name, _notid) {
+
+	var intrfce = document.createElement('iframe');
+	var tool = _tool_name.toString();
+	intrfce.setAttribute('src', 'tools\\' + tool + '\\index.html');
+
 }
---
+++ z_oldVersions/LocalServe.py
@@ -0,0 +1,18 @@
+'''
+LocalServe
+==========
+
+for TOOLS
+
+License - TheRunningStef - 2019
+'''
+
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/output")
+def output():
+	return "Hello World!"
+
+if __name__ == "__main__":
+	app.run("0.0.0.0", "5010")
